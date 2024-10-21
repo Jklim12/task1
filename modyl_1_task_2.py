6f4970bb45d8f0c153f1e2345d3d29e7ea65e99a
Jklim12 <julianova1987@gmail.com>
2024-10-21 17:10:36 +0300

    Add files via upload

---
+++ modyl_1_task_2.py
@@ -0,0 +1,5 @@
+Number_of_homework_completed=12
+Number_of_hours_spent=1,5
+Course_name='Python'
+Time_per_task=0.125
+print(Course_name,Number_of_homework_completed,Number_of_hours_spent,Time_per_task )
